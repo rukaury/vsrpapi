00ba489c5a2fe650c33ad577a75d656ebc41d444
Clements-M <matthew.sta.clements@gmail.com>
2019-06-07 14:21:05 -0400

    add course helper, update model

--- app/course/helper.py
+++ app/course/helper.py
@@ -1,3 +1,56 @@
+from app.models.course import Course
+
+
+def response_for_course(course):
+    """
+    Return the response for when a single course was requested by the user.
+    :param course:
+    :return:
+    """
+    return make_response(jsonify({
+        'status': 'success',
+        'program': course
+    }))
+
+
+def response(status, message, http_code):
+    """
+    Helper method to make a http response
+    :param status: Status message
+    :param message: Response message
+    :param http_code: Response status http_code
+    :return: Http Response
+    """
+    return make_response(jsonify({
+        'status': status,
+        'message': message
+    })), http_code
+
+
+def get_courses_json_list(all_courses):
+    """
+    Make json objects of the school's courses and add them to a list.
+    :param all_courses: Course
+    :return:
+    """
+    courses = []
+    for course in all_courses:
+        courses.append(course.json())
+    return courses
+
+
+def get_courses():
+    """
+    Get a list of all courses
+    :return: courses.
+    """
+    items = None
+    try:
+        return Course.nodes.all()
+    except DoesNotExist:
+        return None
+
+
 '''
 
 def create_course(code, school_id):
--- app/models/course.py
+++ app/models/course.py
@@ -9,3 +9,22 @@ class Course(StructuredNode):
 
     # traverse outgoing TAUGHT_AT relationship, inflate School the course is taught at
     taught_at = RelationshipTo(School, 'TAUGHT_AT')
+
+    def update(self, code, rel):
+        """
+        Function will update the course
+        :param code: the new course code
+        """
+
+        self.code = code if code is not None else self.code
+        rel.updated_on = datetime.datetime.now()
+        self.save()
+
+    def json(self):
+        """
+        Json representation of the course model.
+        :return code: the course code
+        """
+        return {
+            'code': self.code
+        }
