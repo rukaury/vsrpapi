c314f7f9715da45f5aee05b063b6b96bd3a4a3e0
Clements-M <matthew.sta.clements@gmail.com>
2019-06-11 09:25:12 -0400

    big fix

--- app/models/question.py
+++ app/models/question.py
@@ -11,7 +11,8 @@ class Question(StructuredNode):
     text = StringProperty()
     is_multiple_choice = BooleanProperty()
     # traverse the ASKED_BY relationship, inflate the quiz this question belongs to
-    quiz = RelationshipTo("app.models.quiz.Quiz", "ASKED_BY" cardinality=One, model=BaseRel)
+    quiz = RelationshipTo("app.models.quiz.Quiz",
+                          "ASKED_BY", cardinality=One, model=BaseRel)
     # traverse the ANSWERED_FOR relationship, inflate the answers to the question
     answers = RelationshipFrom(
         "app.models.question.Question", "ANSWER_FOR", cardinality=One, model=BaseRel)
